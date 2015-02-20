01d888be48a426bcbf02110ff6bed3493418ed41
Irvin Denzel Torcuato <irvin@wiwi.ninja>
2015-02-20 20:59:19 +0800

    Font awesome added in `grunt serve:dist`.

--- Gruntfile.js
+++ Gruntfile.js
@@ -315,6 +315,12 @@ module.exports = function (grunt) {
           cwd: '.tmp/images',
           dest: '<%= yeoman.dist %>/images',
           src: ['generated/*']
+        }, {
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/font-awesome',
+          src: ['fonts/*.*'],
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
